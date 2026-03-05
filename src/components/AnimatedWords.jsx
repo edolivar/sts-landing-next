c81206e3f813fe3375ddf6710031d883fe61062c
ebeltransts <ebeltran@stsit.biz>
2026-03-05 11:58:44 -0500

    Prueba

--- src/components/AnimatedWords.jsx
+++ src/components/AnimatedWords.jsx
@@ -5,7 +5,7 @@ const AnimatedWords = () => {
         <div className="text-center text-5xl font-bold">
           <div className="relative inline-grid grid-cols-1 grid-rows-1 gap-12 overflow-hidden">
             <span className="animate-word col-span-full row-span-full">Protect</span>
-            <span className="animate-word-delay-1 col-span-full row-span-full">Automate</span>
+            <span className="animate-word-delay-1 col-span-full row-span-full">Automates</span>
             <span className="animate-word-delay-2 col-span-full row-span-full">Innovate</span>
             <span className="animate-word-delay-3 col-span-full row-span-full">STS</span>
           </div>
